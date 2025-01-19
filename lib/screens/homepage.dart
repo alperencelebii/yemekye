5f87d5ebf6395015cdf8bbad95d5ae01370eefc1
Alperen Çelebi <alp.2003c4@gmail.com>
2025-01-19 23:36:15 +0300

    s
    s

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:yemekye/screens/addproduct.dart';
 import 'package:yemekye/screens/restaurant_details.dart';
 import 'package:yemekye/components/models/restaurant_list_card.dart';
@@ -16,6 +18,85 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<String> categories = ['Pastane', 'Kafe', 'FNK', 'Döner'];
   int selectedCategoryIndex = 0;
   String searchQuery = '';
+  late GoogleMapController _mapController;
+  Set<Marker> _markers = {};
+  LatLng? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  // Konum alımı ve markerların Firebase'ten yüklenmesi
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Konum servislerini kontrol et
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    // Konum izni kontrolü
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // Anlık konumu al
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      _currentPosition = LatLng(position.latitude, position.longitude);
+    });
+
+    // Marker'ları yükle
+    _loadMarkersFromFirebase();
+  }
+
+  // Firebase'den markerları alıp haritaya ekler
+  Future<void> _loadMarkersFromFirebase() async {
+    if (_currentPosition == null) return;
+
+    // Firebase 'markers' koleksiyonundan verileri al
+    final snapshot =
+        await FirebaseFirestore.instance.collection('markers').get();
+
+    setState(() {
+      _markers.clear(); // Mevcut markerları temizle
+      for (var doc in snapshot.docs) {
+        final data = doc.data();
+        final latitude = data['latitude'];
+        final longitude = data['longitude'];
+
+        // Null kontrolü yapalım
+        if (latitude != null && longitude != null) {
+          final shopPosition = LatLng(latitude, longitude);
+
+          final marker = Marker(
+            markerId: MarkerId(doc.id),
+            position: shopPosition,
+            infoWindow: InfoWindow(
+              title: data['name'] ?? 'Mağaza Adı Yok',
+              snippet: data['address'] ?? 'Adres Bilgisi Yok',
+            ),
+          );
+          _markers.add(marker);
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -171,8 +252,41 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               const SizedBox(height: 10),
+              // Harita
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: _currentPosition == null
+                    ? const Center(child: CircularProgressIndicator())
+                    : GoogleMap(
+                        onMapCreated: (controller) {
+                          _mapController = controller;
+                        },
+                        markers:
+                            _markers, // Firebase'ten çekilen markerlar burada görünecek
+                        initialCameraPosition: CameraPosition(
+                          target: _currentPosition!,
+                          zoom: 14.0,
+                        ),
+                        myLocationEnabled: true,
+                        myLocationButtonEnabled: true,
+                      ),
+              ),
+              const SizedBox(height: 10),
+              // Restoranlar
               StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance.collection('shops').snapshots(),
+                stream:
+                    FirebaseFirestore.instance.collection('shops').snapshots(),
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return const Center(child: CircularProgressIndicator());
